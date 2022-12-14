580bafece4355350b793faf4d03373b16a923069
SunyataZero <tord@disroot.org>
2022-12-14 10:45:10 +0100

    WIP: #28

--- website/admin.py
+++ website/admin.py
@@ -5,9 +5,9 @@ from . import models
 
 @gis_admin.register(models.Location)
 class LocationAdmin(gis_admin.OSMGeoAdmin):
-    list_display = ("address", "coordinates")
+    list_display = ("id", "title", "coordinates", "initiative")
 
 
 @gis_admin.register(models.Initiative)
 class InitiativeAdmin(gis_admin.ModelAdmin):
-    pass
+    list_display = ("id", "title")
