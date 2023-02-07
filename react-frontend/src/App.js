0f7e59fa3767cf2151fc3a9542f10fb614c89a6e
SunyataZero <tord@disroot.org>
2023-02-07 15:29:01 +0100

    Added sitemap and updated the tag page to include list of Initiatives

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Details from "./pages/Details";
 import TagPage from "./pages/TagPage";
 import PageNotFound from "./pages/PageNotFound";
+import Sitemap from "./pages/Sitemap";
 
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<Home/>}/>
                     <Route path="/details/:initiativeId" element={<Details/>}/>
+                    <Route path="/sitemap" element={<Sitemap/>}/>
                     <Route path="/tag/:tagId" element={<TagPage/>}/>
                     <Route path="/r/:regionSlug" element={<Home/>}/>
                     <Route path="*" element={<PageNotFound/>}/>
--- react-frontend/src/pages/TagPage.js
+++ react-frontend/src/pages/TagPage.js
@@ -1,6 +1,21 @@
 import React, {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 
+function renderInitiatives(tag) {
+    console.log("renderTags")
+    if (Object.hasOwn(tag, 'initiatives') && typeof tag.initiatives != 'undefined') {
+        console.log(tag.initiatives)
+        // const tag_href_str=`${process.env.REACT_APP_BACKEND_URL}/tags/${tagElement.id}`;
+        return tag.initiatives.map(
+            (initiativeElement) => (
+                <li key={initiativeElement.id}>
+                    <a href={`/details/${initiativeElement.id}`}>{initiativeElement.initiative_title_texts[0].text}</a>
+                </li>
+            )
+        );
+    }
+}
+
 const TagPage = () => {
     const {tagId} = useParams();
 
@@ -23,7 +38,7 @@ const TagPage = () => {
             <h3>ID: {tagId}</h3>
             <h3>Title: {tag.title}</h3>
             <ul>
-                <li>test</li>
+                {renderInitiatives(tag)}
             </ul>
         </div>
     );
