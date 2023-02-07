24cdc4ebf964288f9163ee04e88d171bb6f8fc86
SunyataZero <tord@disroot.org>
2023-02-07 14:57:41 +0100

    Added links to each tag on details pages

--- react-frontend/src/pages/Details.js
+++ react-frontend/src/pages/Details.js
@@ -1,6 +1,22 @@
 import React, {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 
+function renderTags(initiative) {
+    console.log("renderTags")
+    if (Object.hasOwn(initiative, 'tags') && typeof initiative.tags != 'undefined') {
+        console.log(initiative.tags)
+        // const tag_href_str=`${process.env.REACT_APP_BACKEND_URL}/tags/${tagElement.id}`;
+        return initiative.tags.map(
+            (tagElement) => (
+                <li key={tagElement.id}>
+                    <a href={`/tag/${tagElement.id}`}>{tagElement.title}</a>
+                </li>
+            )
+        );
+    }
+}
+
+
 const Details = () => {
     const {initiativeId} = useParams();
 
@@ -17,12 +33,17 @@ const Details = () => {
             });
     }, []);
 
+    const renderedTags=renderTags(initiative);
     return (
         <div>
             <h2>Details page for Initiative</h2>
             <h3>ID: {initiativeId}</h3>
             <h3>Name: {initiative.name}</h3>
             <p>Description: {initiative.description}</p>
+            <h3>Tags:</h3>
+            <ul>
+                {renderTags(initiative)}
+            </ul>
         </div>
     );
 };
