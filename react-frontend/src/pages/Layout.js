010c53b3ce329931d4c0d5e0e9d4145235238166
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2023-02-22 12:01:33 +0100

    move sitemap down

--- react-frontend/src/pages/Layout.js
+++ react-frontend/src/pages/Layout.js
@@ -5,17 +5,13 @@ const Layout = () => {
     return (
         <div>
             <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/sitemap">Sitemap</Link>
-                    </li>
-                </ul>
             </nav>
 
             <Outlet />
+
+            <div id="footer">
+                        <Link to="/sitemap">Sitemap</Link>
+            </div>
         </div>
     );
 };
