c79f094e2c6ac2d0353398c0c5425be8f1e8a112
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2023-05-17 09:29:17 +0200

    first variant of footer
    for #124

--- react-frontend/src/pages/Layout.tsx
+++ react-frontend/src/pages/Layout.tsx
@@ -9,8 +9,32 @@ export function Layout() {
 
             <Outlet />
 
-            <div id="footer">
-                        <Link to="/sitemap">Sitemap</Link>
+            <div id="footer" class="row">
+                <div id="footer-sk-kes" className='col-md-3'>
+                    Smartakartan
+                    by
+                    KES
+
+                    Made with Love at GBG
+                    
+                    Source Code
+                </div>
+                <div id="footer-regions-kes" className='col-md-3'>
+                    <h5>Participating regions </h5>
+                    <ul>
+                        {regions.map((region) => 
+                        <li><Link to={'/r/'+region.properties.slug}>
+                            {region.properties.title}
+                        </Link></li>
+                        )}
+                    </ul>
+                </div>
+                <div id="footer-region-pages" className="col-md-3">
+
+                </div>
+                <div id="footer-other-pages" className="col-md-3">
+                    <Link to="/sitemap">Sitemap</Link>
+                </div>
             </div>
         </div>
     );
