3595d8dcc4506c649e587cdeecd487c10eafce5a
D. Freiermuth <d_freiermu14@cs.uni-kl.de>
2023-05-21 13:21:10 +0200

    make footer nicer

--- react-frontend/src/pages/Layout.tsx
+++ react-frontend/src/pages/Layout.tsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Outlet, Link } from "react-router-dom";
 import { Language, Region, fetchLanguages } from '../KesApi';
 import NavBar from '../components/NavBar';
+import styled from 'styled-components';
+
+const Footer = styled.div`
+    padding-top: 2rem;
+`
 
 export function Layout({regions} : {regions:Region[]}) {
     const [languages, setLanguages] = useState<Language[]>([]);
@@ -9,6 +14,7 @@ export function Layout({regions} : {regions:Region[]}) {
         fetchLanguages().then(l => setLanguages(l));
     }, []);
     return (
+        <>
         <div className="container">
             <nav>
             <NavBar
@@ -18,34 +24,60 @@ export function Layout({regions} : {regions:Region[]}) {
 
             <Outlet />
 
-            <div id="footer" className="row">
-                <div id="footer-sk-kes" className='col-md-3'>
-                    Smartakartan
-                    by
-                    KES
-
-                    Made with Love at GBG
-                    
-                    Source Code
+            <Footer className="row footer ng-scope pt-10" ng-controller="Footer">
+                <div className="col-lg-offset-3 col-xs-6 col-sm-3 col-md-offset-1 col-md-3 col-lg-3 company">
+                    <img style={{width:'6rem', padding:'0.5rem'}} src="/sk-logo.svg"/>
+                    <p><strong style={{fontFamily: "'Open Sans', sans-serif"}}>smartakartan.se</strong></p>
+                    <p className="text-muted ng-scope">Crafted with <span className="fa fa-heart animated-hover faa-fast faa-pulse"></span> in GBG, Sverige</p>
+                    <p className="text-muted socials">
+                    <a href="https://kollaborativekonomi.se">
+                        <img style={{width: '2.8rem'}} src="/KES_with_border.png"/>
+                    </a>
+                    <a href="https://www.facebook.com/smartakartan.se" target="_blank" className="facebook" title="Facebook">
+                        <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x"></i>
+                        <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </a>
+                    <a href="https://www.linkedin.com/company/kollaborativ-ekonomi" target="_blank" className="linkedin" title="Linkedin">
+                        <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x"></i>
+                        <i className="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </a>
+                    </p>
+                    <p className="text-muted ng-binding">© 2023 All rights reserved</p>
                 </div>
-                <div id="footer-regions-kes" className='col-md-3'>
-                    <h5>Participating regions </h5>
-                    <ul>
-                        {regions.map((region) => 
-                        <li><Link to={'/r/'+region.properties.slug}>
-                            {region.properties.title}
-                        </Link></li>
-                        )}
+                <div className="col-lg-2 col-md-2 col-xs-6 col-sm-3">
+                    <ul className="list-unstyled">
+                    <li><strong>Smartakartan</strong></li>
+                    <li><a href="https://kollaborativekonomi.se" target="_blank" className="ng-scope">KES</a></li>
+                    <li><a href="https://www.smartakartan.se/om-smarta-kartan" target="_blank" className="ng-scope">About the map</a></li>
+                    <li><a href="https://www.smartakartan.se/vardegrund" target="_blank" className="ng-scope">Our values</a></li>
+                    <li><a href="https://gitlab.com/kollaborativ-ekonomi/smartakartan4" className="ng-scope">Open source</a></li>
                     </ul>
                 </div>
-                <div id="footer-region-pages" className="col-md-3">
-
+                <div className="col-lg-2 col-md-2 col-xs-6 col-sm-3">
+                    <ul className="list-unstyled">
+                    <li><strong>Regions</strong></li>
+                    {regions.map((region) => 
+                    <li><Link to={'/r/'+region.properties.slug}>
+                        {region.properties.title}
+                    </Link></li>
+                    )}
+                    </ul>
                 </div>
-                <div id="footer-other-pages" className="col-md-3">
-                    <Link to="/sitemap">Sitemap</Link>
+                <div className="col-lg-2 col-md-2 col-xs-6 col-sm-3">
+                    <ul className="list-unstyled">
+                    <li><strong className="ng-scope">Legal</strong></li>
+                    <li><a ui-sref="terms" className="ng-scope" href="#!/terms">Terms of service</a></li>
+                    <li><Link to="/sitemap">Sitemap</Link></li>
+                    </ul>
                 </div>
-            </div>
+                <div className="clearfix visible-xs-block"></div>
+            </Footer>
         </div>
+        </>
     );
 }
 
