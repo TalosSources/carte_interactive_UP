b59355d9906dc9ae692307a0b83cd3c4aab19ad2
llllaszlo <laszlo.baudriller@epfl.ch>
2025-08-05 18:46:34 +0200

    test new icon

--- main.js
+++ main.js
@@ -11,7 +11,18 @@ fetch('data/places.json')
     renderTagFilters();
     filterPlaces(); // initial render
     });
-
+var LeafIcon = L.Icon.extend({
+    options: {
+        iconSize:     [38, 95],        // taille de l'icône
+        shadowSize:   [50, 64],        // taille de l'ombre
+        iconAnchor:   [22, 94],        // point de l'icône correspondant à la position du marqueur
+        shadowAnchor: [4, 62],         // idem pour l'ombre
+        popupAnchor:  [-3, -76]        // point à partir duquel la popup s'ouvre par rapport à l'icône
+    }
+});
+var greenIcon = new LeafIcon({
+    iconUrl: 'https://raw.githubusercontent.com/TalosSources/carte_interactive_UP/refs/heads/static/NEW%20ICON-01.png',
+});
 function initMap() {
     map = L.map('map', {maxZoom: 16}).setView([46.5300, 6.61011], 13);
     
@@ -54,7 +65,7 @@ function initMap() {
 function fitMapToPlaces(places) {
   if (places.length === 0) return;
   const group = new L.featureGroup(
-    places.map(p => L.marker([p.lat, p.lng]))
+    places.map(p => L.marker([p.lat, p.lng]{ icon: greenIcon })))
   );
   // TODO: Max zoom, to avoid getting too close? 
   map.fitBounds(group.getBounds(), { padding: [20, 20] });
